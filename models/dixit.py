df627cdf525c4e6f4744ba4b8b243326963193b9
Diego Pereyra <diesphink@gmail.com>
2023-10-13 00:05:31 -0300

    Using dimensions from sphlib

--- models/dixit.py
+++ models/dixit.py
@@ -1,40 +1,40 @@
 from ocp_vscode import show
-from sphlib import align
+from sphlib import Dimensions, align
 
 from build123d import Axis, Box, chamfer, fillet
 
 
 X, Y, Z = 0, 1, 2
 
-d = {
-    "module": [135, 90, 54],
-    "wall": 3,
-    "floor": 0.4,
-    "card": [126, 84, 36 / 100],
-    "score": [118, 250, 6],
-    "guess": [118, 76, 22],
-    "token": [50, 64, 24],
-}
+d = Dimensions()
+d.module = [135, 90, 54]
+d.wall = 3
+d.floor = 0.4
+d.card = [126, 84, 36 / 100]
+d.score = [118, 250, 6]
+d.guess = [118, 76, 22]
+d.token = [50, 64, 24]
 
-module = Box(*d["module"])
 
-cards_slot = Box(d["card"][X], d["card"][Y], d["module"][Z] - d["floor"])
-finger_slot = Box(25, 25, d["module"][Z] - d["floor"])
-score_slot = Box(*d["score"])
-guess_slot = Box(*d["guess"])
-token_slot = Box(*d["token"])
+module = Box(*d.module)
 
-module -= align(cards_slot, module, begin="x", end="z", margins=[d["wall"], 0, 0])
+cards_slot = Box(d.card[X], d.card[Y], d.module[Z] - d.floor)
+finger_slot = Box(25, 25, d.module[Z] - d.floor)
+score_slot = Box(*d.score)
+guess_slot = Box(*d.guess)
+token_slot = Box(*d.token)
+
+module -= align(cards_slot, module, begin="x", end="z", margins=[d.wall, 0, 0])
 module -= align(finger_slot, module, center="y", end="xz")
 
 module2 = module.copy()
 module2 -= align(score_slot, module2, center="xy", end="z")
 module2 = align(module2, module, beginToEnd="y", margin=2)
 
-module3 = Box(*d["module"])
+module3 = Box(*d.module)
 module3 = align(module3, module2, beginToEnd="y", margin=2)
 module3 -= align(score_slot, module3, center="xy", end="z")
-guess_slot = align(guess_slot, module3, center="xy", end="z", margins=[0, 0, d["score"][Z]])
+guess_slot = align(guess_slot, module3, center="xy", end="z", margins=[0, 0, d.score[Z]])
 module3 -= guess_slot
 module3 -= align(finger_slot, guess_slot, begin="z", beginToEnd="x", center="y")
 module3 -= align(token_slot, guess_slot, endToBegin="z", begin="x", center="y", margins=[6, 0, 0])
