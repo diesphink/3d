24b02165384b558383bbdfb4762ed1dd0db9ccbd
Diego Pereyra <diesphink@gmail.com>
2023-10-21 14:30:53 -0300

    cleanups

--- models/caixa_brasil.py
+++ models/caixa_brasil.py
@@ -1,56 +1,29 @@
 from ocp_vscode import show
 
 from build123d import (
-    Axis,
-    BuildLine,
     BuildPart,
     BuildSketch,
     Mode,
     Plane,
-    Rectangle,
-    Vector,
     extrude,
     import_svg,
     make_face,
     mirror,
     offset,
-    scale,  
+    scale,
 )
 
 parede, altura, largura, tolerancia = 2, 10, 100, 0.1
 with BuildPart() as box:
     with BuildSketch() as mapa:
-        make_face(import_svg("./src/assets/path6858.svg"))
+        make_face(import_svg("./assets/path6858.svg"))
         scale(by=largura / mapa.sketch.bounding_box().size.X)
         mirror(about=Plane.XZ, mode=Mode.REPLACE)
     print(mapa.sketch.location)
     extrude(amount=altura)
-    
+
     inner_map = offset(mapa.sketch, amount=-parede)
     extrude(to_extrude=inner_map, amount=altura - parede, mode=Mode.SUBTRACT)
-    
+
 
 show(box)
-
-# face = make_face(edges)
-# face = scale(objects=face, by=largura / face.bounding_box().size.X)
-# box = extrude(to_extrude=face, amount=altura)
-# inner_face = offset(face.sketch, amount=-parede)
-
-
-
-# inner_box = extrude(inner_face, amount=altura - parede)
-# box = box.cut(inner_box)
-
-# face = face.translate(vector=Vector(largura * 1.1, 0, 0))
-# inner_face = offset(face, amount=-parede - tolerancia) 
-
-# lid = extrude(to_extrude=face, amount=parede)
-# inner_lid = extrude(inner_face, amount=parede * 2)
-# lid = lid.fuse(inner_lid)
-
-# show([box, lid])
-# print(box.bounding_box().size)
-
-# box.export_stl("library/caixa_brasil/box.stl")
-# lid.export_stl("library/caixa_brasil/lid.stl")
--- models/slartibartfast.py
+++ models/slartibartfast.py
@@ -1,5 +1,5 @@
 from ocp_vscode import show
-from honeycomb import honeycomb_pins
+from honeycomb import HoneycombGrid
 from sphlib import Dimensions, align
 
 from build123d import Box, Cylinder, Part, Plane, Rot, chamfer, fillet
@@ -12,7 +12,7 @@ d.margins1 = [4.5, 97 - 7 - 84.5, 0]
 d.margins2 = [3, 7, 0]
 
 base = Box(*d.base)
-base += align(Plane.XY * Rot(0, 180, 90) * honeycomb_pins([3, 3, 3, 3, 3, 3, 3]), ref=base, center="xy", endToBegin="z")
+base += align(Plane.XY * Rot(0, 180, 90) * HoneycombGrid([3, 3, 3, 3, 3, 3, 3]), ref=base, center="xy", endToBegin="z")
 base -= align(Cylinder(radius=d.radius_screw, height=20), ref=base, centerToBegin="xy", center="z", margins=d.margins1)
 base -= align(Cylinder(radius=d.radius_screw, height=20), ref=base, centerToEnd="xy", center="z", margins=d.margins2)
 show(base)
