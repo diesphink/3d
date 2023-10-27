e4ede932f45130ed8c4eb9d2882d7e348fed12e6
Diego Pereyra <diesphink@gmail.com>
2023-10-27 13:27:37 -0300

    Fixed model

--- models/gf_battery.py
+++ models/gf_battery.py
@@ -14,18 +14,5 @@ d.charger = [65, 95, 28]
 base = gf.GridfinityFilled(x_grid_number=2, y_grid_number=3, unit_height=5, disable_mholes=True)
 slot = Slot(Box(*d.charger), SlotPosition.Y_AXIS, 30, SlotType.SPHERE)
 base -= align(slot, ref=base, center="xy", end="z", margin=3.85)
-# show(base)
-
-show(
-    GridfinityBox(
-        x_grid_number=3,
-        y_grid_number=3,
-        y_divider_number=2,
-        x_divider_number=2,
-        unit_height=5,
-        disable_mholes=True,
-        disable_scoops=True,
-    )
-)
-
+show(base)
 base.export_stl("library/gridfinity/battery/charger.stl")
