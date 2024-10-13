463f3208a2156bfed254151839f63dc26f557150
Diego Pereyra <diesphink@gmail.com>
2024-10-13 01:13:04 -0300

    exporta métodos no package

--- sphlib/__init__.py
+++ sphlib/__init__.py
@@ -1,3 +1,5 @@
 from .align import align
+from .distribute import distribute
 from .dimensions import Dimensions
 from .slots import FingerSlot, Slot, SlotPosition, SlotType
+from .chamfer import rescale_chamfer
